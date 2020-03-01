3c65fb2e85100bb2b6352bf7e6215afe15e65222
seokjoon <seokjoon@gmail.com>
2020-03-01 15:21:32 +0900

    inject

--- src/test/index.js
+++ src/test/index.js
@@ -1,16 +1,58 @@
-import React, {Component} from 'react';
+import React, { Component, Fragment } from 'react'
 
-import {WebView} from 'react-native-webview';
+import { AppState, Button, Modal, Text, View } from 'react-native'
+import { WebView } from 'react-native-webview'
 
 class Index extends Component {
-  render() {
-    return (
-      <WebView
-        source={{uri: 'https://prime.epikar.com/app'}}
-        // style={{marginTop: 20}}
-      />
-    );
+  constructor () {
+    super()
+  }
+
+  state = {
+    appState: AppState.currentState,
+  }
+
+  componentDidMount () {
+    AppState.addEventListener('change', this.reload)
+  }
+
+  componentWillUnmount () {
+    AppState.removeEventListener('change', this.reload)
+  }
+
+  params = () => {
+    const date = Math.floor(new Date().getTime() / 1000);
+    return `
+      document.appUserPlusId = 't_plusId_${date}'; 
+      document.appUserName = 't_user_${date}'; 
+      document.appVehicleNumId = 't_numId_${date}';
+      document.appVehicleNumReg = 't_numId_${date}';
+      true;
+    `
+  };
+
+  reload = nextAppState => {
+    if (this.state.appState.match(/inactive|background/) && nextAppState === 'active') this.webview.reload()
+    this.setState({ appState: nextAppState })
+  }
+
+  render () {
+    const outs = (
+      <Fragment>
+        <WebView
+          //source={{uri: 'https://prime.epikar.com/app'}}
+          source={{ uri: 'http://192.168.0.2:8081' }}
+          // style={{marginTop: 20}}
+          ref={r => (this.webview = r)}
+          injectedJavaScript={this.params()}
+          key={this.state.appState}
+          // onNavigationStateChange={() => { alert('change'); }}
+        />
+      </Fragment>
+    )
+
+    return (<Fragment>{outs}</Fragment>)
   }
 }
 
-export default Index;
+export default Index
